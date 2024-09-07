4e4c39c82b1848bfa74b226945eefc7085f07ad8
Harsh <hsharmarishra@gmail.com>
2024-09-07 11:57:42 +0530

    Created backend for medvision

---
+++ server.js
@@ -0,0 +1,17 @@
+
+const express = require("express");
+const app = express();
+// require("./models/dbconnect"); // Ensure this connects to your MongoDB
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send("Hello Server here!!");
+});
+
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+    console.log(`Server is listening at port number ${port}`);
+});
